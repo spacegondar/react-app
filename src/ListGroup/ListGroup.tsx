408e9380756dd6005ac013332399c6e9842db01a
Uzay Peker <pekeruzay@hotmail.com>
2024-10-06 00:53:06 +0300

    Conditional Render

--- src/ListGroup/ListGroup.tsx
+++ src/ListGroup/ListGroup.tsx
@@ -1,21 +1,33 @@
-function ListGroup() {
-  let items = ["Paris", "London", "Tokyo"];
-  items = [];
+import { MouseEvent } from "react";
 
-  const conditionalHtmlElement = (
-    <>
-      <h1>Test</h1>
-      <p>Paragraphs</p>
-    </>
-  );
+function ListGroup() {
+  const items = ["Paris", "London", "Tokyo"];
+
+  const getMessage = (countryName: string) => {
+    return (
+      items.length === 0 && (
+        <>
+          <h1>Test</h1>
+          <p>{countryName}</p>
+        </>
+      )
+    );
+  };
+
+  const handleClick = (event: MouseEvent) => console.log(event.target.value);
 
   return (
     <>
       <h1>Testo</h1>
-      {items.length === 0 ? conditionalHtmlElement : null}
+      {getMessage("Franceq")}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            value={item + index}
+            className="list-group-item"
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
