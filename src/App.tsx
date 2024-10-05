aaf00886516a55916c1ef70e6a50979c9c5dbaf5
Uzay Peker <pekeruzay@hotmail.com>
2024-10-06 01:27:26 +0300

    Send data to Parent

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import ListGruop from "./ListGroup";
 
 function App() {
   const cities = ["London", "Paris", "Istanbul", "Berlin"];
+
+  const handleSelectItem = (item: string) => {
+    console.log("On Event Fire", item);
+  };
+
   return (
     <div>
-      <ListGruop items={cities} heading="Cities" />
+      <ListGruop
+        items={cities}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/ListGroup/ListGroup.tsx
+++ src/ListGroup/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react"; //MouseEvent,
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState<number | string>();
   const getMessage = (countryName: string) => {
     return (
@@ -33,7 +34,10 @@ function ListGroup({ items, heading }: Props) {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
